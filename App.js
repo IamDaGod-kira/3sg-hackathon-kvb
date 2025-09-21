b84c4bf954612127853505525100c060588da579
IamDaGod-kira <IamDaGod_MoonDeity@protonmail.com>
2025-09-21 20:47:16 +0530

    still cant fix nativewind

--- App.js
+++ App.js
@@ -1,22 +1,11 @@
-import './App.css';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className="text-xl font-bold text-blue-500">Welcome to the Basic Page!</Text>
+    <View className="items-center justify-center flex-1">
+      <Text className="text-xl font-bold text-amber-200 bg-slate-900">Welcome to the Basic Page!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'blue',
-  },
-});
+};
