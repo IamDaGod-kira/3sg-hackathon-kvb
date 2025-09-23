b93847512c4f52a3d004fb572216e7ec750f146e
IamDaGod-kira <IamDaGod_MoonDeity@protonmail.com>
2025-09-23 07:33:49 +0530

    added login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import './App.css'
+import bareIcon from './assets/bare-icon.png';
+import kvLogo from './assets/kv-logo.png';
+import vidyasetuLogo from './assets/vidyasetu-logo.png';
 
 function App() {
   return (
@@ -8,10 +11,32 @@ function App() {
         <p className='text-1.5xl p-5 italic'>A <a href='https://www.vidyasetu.ai/'>Hackathon</a> project by <a href="https://github.com/IamDaGod-kira/">CodeCrafters</a></p>
         <br />
         <h2
-          className="text-1.5xl border-4 border-dotted border-indigo-800 rounded-lg p-2"
+          className="text-1.5xl border-4 border-double border-indigo-800 rounded-lg p-2"
         >
-          Login
+          Login to the App
         </h2>
+        <br />
+        <input
+          className="min-w-full mb-4 px-4 py-2 rounded bg-purple-100 text-purple-900 placeholder-purple-400 focus:outline-none focus:ring-2 focus:ring-purple-500 caret-purple-700 hover:border-emerald-600 hover:border-2"
+          type="email"
+          name="email"
+          placeholder="Enter your Email"
+        />
+        <br />
+        <input
+          className="min-w-full px-4 py-2 rounded bg-purple-100 text-purple-900 placeholder-purple-400 focus:outline-none focus:ring-2 focus:ring-purple-500 caret-purple-700 hover:border-emerald-600 hover:border-2"
+          type="password"
+          name="passwd"
+          placeholder="Enter your Password"
+        />
+        <br /><br />
+        <button
+          type="submit"
+          className="text-slate-900 border-3px-indigo-800 hover:border-emerald-600"
+          style={{ backgroundColor: "#FFFDD0" }}
+        >
+          Login ≫
+        </button>
       </div>
     </div>
   )
