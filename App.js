632411c19f7a3a5648eef8f359fab88df7437f4f
IamDaGod-kira <IamDaGod_MoonDeity@protonmail.com>
2025-10-03 14:13:48 +0530

    nothing after an hour... i need help

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Text, View } from "react-native";
 export default function App() {
   return (
-    <View className="items-center justify-center flex-1">
-      <Text className="text-xl font-bold text-amber-200 bg-slate-900">Welcome to the Basic Page!</Text>
-      <StatusBar style="auto" />
+    <View className="flex-1 items-center justify-center bg-blue-500">
+      <Text className="text-white text-lg font-bold">
+        Hello Tailwind v4 + Expo 👋
+      </Text>
     </View>
   );
-};
+}
